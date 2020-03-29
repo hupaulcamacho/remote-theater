0e702676d04ea74c6a241e989a4b0b5459d46fbb
kadjiah <kadijahwilson@pursuit.org>
2020-03-29 14:01:24 -0400

    added missing semi colons

--- server/database/database.sql
+++ server/database/database.sql
@@ -9,7 +9,7 @@ CREATE TABLE genre
 (
     genre_id SERIAL PRIMARY KEY,
     genre_name VARCHAR
-)
+);
 
 CREATE TABLE videos
 (
@@ -36,7 +36,7 @@ CREATE TABLE showrooms
     showroom_id SERIAL PRIMARY KEY,
     title VARCHAR,
     video_id INT REFERENCES videos(video_id)
-)
+);
 
 CREATE TABLE comments
 (
@@ -45,7 +45,7 @@ CREATE TABLE comments
     users_id INT REFERENCES users(users_id),
     video_id INT REFERENCES videos(video_id),
     showroom_id INT REFERENCES showrooms(showroom_id)
-)
+);
 
 CREATE TABLE preferences
 (
@@ -53,13 +53,13 @@ CREATE TABLE preferences
     age VARCHAR(2),
     users_id INT REFERENCES users(users_id),
     genre_name INT REFERENCES genre(genre_name)
-)
+);
 
 CREATE TABLE viewer
 (
     users_id INT REFERENCES users(users_id),
     showroom_id INT REFERENCES showrooms(showroom_id)
-)
+);
 
 INSERT INTO genre
     (genre_name)
@@ -73,34 +73,34 @@ VALUES
     ('Romantic');
 
 INSERT INTO videos
-    (title, genre_id, description, video_url)
+    (video_id, title, genre_id, description, video_url)
 VALUES
-    ('The Invisible Man', 5, 'The Invisible Man is a 2020 science fiction horror film written and directed by Leigh Whannell. It follows a woman who, after the apparent suicide of her abusive and wealthy boyfriend, believes she is being stalked by him.', 'https://www.youtube.com/watch?v=Pso0Aj_cTh0'),
-    ('Survive The Nght', 1, '', 'https://www.youtube.com/watch?v=TfTNGqXBwHY&list=PLScC8g4bqD45-Bue4BX7U2h4IkzEV3hcL&index=1'),
-    ('Whats The Worst That Could Happen?', 3, 'A rich man catches a thief burglarizing his home and steals the thiefs lucky ring, who then tries to get it back.', 'youtube.com/watch?v=9LaZ6pqSnqM'),
-    ('Mulan', 2, 'A young Chinese maiden disguises herself as a male warrior in order to save her father. A live-action feature film based on Disney''s Mulan.', 'https://www.youtube.com/watch?v=XrAmQS9jJJo'),
-    ('Barbershop 2: Back in Business', 3, 'The owner of an historic South Side Chicago barbershop is pressured to sell out to a land developer, but must face the impact this would have on his close-knit community.'),
-    ('Rampage', 6, 'Primatologist Davis Okoye shares an unshakable bond with George, an extraordinarily intelligent, silverback gorilla that''s been in his care since birth. When a rogue genetic experiment goes wrong, 
+    (1,'The Invisible Man', 5, 'The Invisible Man is a 2020 science fiction horror film written and directed by Leigh Whannell. It follows a woman who, after the apparent suicide of her abusive and wealthy boyfriend, believes she is being stalked by him.', 'https://www.youtube.com/watch?v=Pso0Aj_cTh0'),
+    (2,'Survive The Nght', 1, '', 'https://www.youtube.com/watch?v=TfTNGqXBwHY&list=PLScC8g4bqD45-Bue4BX7U2h4IkzEV3hcL&index=1'),
+    (3,'Whats The Worst That Could Happen?', 3, 'A rich man catches a thief burglarizing his home and steals the thiefs lucky ring, who then tries to get it back.', 'youtube.com/watch?v=9LaZ6pqSnqM'),
+    (4,'Mulan', 2, 'A young Chinese maiden disguises herself as a male warrior in order to save her father. A live-action feature film based on Disney''s Mulan.', 'https://www.youtube.com/watch?v=XrAmQS9jJJo'),
+    (5,'Barbershop 2 Back in Business', 3, 'The owner of an historic South Side Chicago barbershop is pressured to sell out to a land developer, but must face the impact this would have on his close-knit community.'),
+    (6,'Rampage', 6, 'Primatologist Davis Okoye shares an unshakable bond with George, an extraordinarily intelligent, silverback gorilla that''s been in his care since birth. When a rogue genetic experiment goes wrong, 
     it causes George, a wolf and a reptile to grow to a monstrous size. As the mutated beasts embark on a path of destruction, Okoye teams up with a discredited genetic engineer and the military to secure an antidote and prevent a global catastrophe.', 'https://www.youtube.com/watch?v=coOKvrsmQiI'),
-    ('Bright', 1, 'A detective must work with an Orc to find a powerful wand before evil creatures do.', 'https://www.youtube.com/watch?v=giozR7nb51c'),
-    ('The Godfather', 4, 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.', 'https://www.youtube.com/watch?v=sY1S34973zA' );
+    (7,'Bright', 1, 'A detective must work with an Orc to find a powerful wand before evil creatures do.', 'https://www.youtube.com/watch?v=giozR7nb51c'),
+    (8,'The Godfather', 4, 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.', 'https://www.youtube.com/watch?v=sY1S34973zA' );
 
 INSERT INTO users
-    (name, email, password)
+    (users_id, name, email, password)
 VALUES
-    ('Kadijah Wilson', 'kwilson@pursuit.org', '1234'),
-    ('Hupaul Camacho', 'hcamacho@pursuit.org', '1234'),
-    ('John Doe', 'jdoe@pursuit.org', '1234'),
-    ('Chuck Okonkwo', 'cokonkwo@pursuit.org', '1234'),
-    ('Jane Smith', 'jsmith@pursuit.org', '1234');
+    (1, 'Kadijah Wilson', 'kwilson@pursuit.org', '1234'),
+    (2, 'Hupaul Camacho', 'hcamacho@pursuit.org', '1234'),
+    (3, 'John Doe', 'jdoe@pursuit.org', '1234'),
+    (4, 'Chuck Okonkwo', 'cokonkwo@pursuit.org', '1234'),
+    (5, 'Jane Smith', 'jsmith@pursuit.org', '1234');
 
 INSERT INTO showrooms
-    (title, video_id)
+    (showroom_id, title, video_id)
 VALUES
-    ('Survive The Night', 2),
-    ('The Invisible Man', 1),
-    ('Whats The Worst That Could Happen?', 3),
-    ('Mulan', 4);
+    (1, 'Survive The Night', 2),
+    (2, 'The Invisible Man', 1),
+    (3, 'Whats The Worst That Could Happen?', 3),
+    (4, 'Mulan', 4);
 
 INSERT INTO comments
     (users_id, video_id, showroom_id, comment_body)
