f9097c6b4a2d441e4682a89499f90f3ea6906aa6
kadjiah <kadijahwilson@pursuit.org>
2020-04-15 18:34:18 -0400

    installed cors and added app.use cors to the app.js file

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var cors = require('cors')
 var logger = require('morgan');
 var session = require('express-session');
 var passport = require('./auth/passport');
@@ -15,6 +16,7 @@ let videosRouter = require('./routes/videos');
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
