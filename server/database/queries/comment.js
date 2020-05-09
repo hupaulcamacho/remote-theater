6cdf59331634e4b8ec7ec42202380e3fd5b8e915
kadjiah <kadijahwilson@pursuit.org>
2020-05-09 11:48:59 -0400

    committing changes

--- server/database/queries/comment.js
+++ server/database/queries/comment.js
@@ -13,7 +13,6 @@ getAllComments = async () => {
 getCommentsByVideoID = async (video_id) => {
     try {
         const commentByVideoID = await db.any(`SELECT * FROM comments WHERE video_id = $1`, [video_id])
-        console.log(commentByVideoID)
         return commentByVideoID
     } catch (error) {
         console.log("error", error)
@@ -25,7 +24,6 @@ try{
     const insertQuery = `INSERT INTO comments (id, comment_body) 
     VALUES($1, $2) RETURNING *`
     let response = await db.any(insertQuery, [users_id, body])
-    console.log(insertQuery)
     return response;
 }catch(error){
  console.log(error)
