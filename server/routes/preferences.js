f123f219f00633585aae67c72e284fb413396fda
kadjiah <kadijahwilson@pursuit.org>
2020-05-23 17:28:33 -0400

    separated select statements

--- server/routes/preferences.js
+++ server/routes/preferences.js
@@ -4,15 +4,16 @@ const preferences = require('../database/queries/preferences')
 const db = require('../database/db.js')
 
 router.get('/id/:user_id', async (req, res) => {
-	const id = req.body.user_id
+	const id = req.params.user_id
 	try {
-		let preference = await preferences.getPreferenceByGenreId(id)
+		let preference = await preferences.getPreferenceByUserId(id)
 		res.status(200).json({
 			status: 'success',
 			payload: preference
 		});
 	}
 	catch(err){
+		console.log(err)
 		res.status(500).json({
             payload: null,
             msg: err,
@@ -22,16 +23,14 @@ router.get('/id/:user_id', async (req, res) => {
 });
 
 router.post('/add/:user_id/:genre_id', async (req, res) => {
+	const user_id = req.params.user_id
+	const genre_id = req.params.genre_id
 	try {
-		let preferenceQuery = 'SELECT * FROM preferences WHERE user_id = $1 AND genre_id = $2';
-		let preferenceInsertQuery = 'INSERT INTO preferences (user_id, genre_id) VALUES ($1, $2)'
-		let preference = await db.oneOrNone(preferenceQuery, [req.params.user_id, req.params.genre_id]);
-		if (!preference) {
-			await db.none(preferenceInsertQuery, [req.params.user_id, req.params.genre_id]);
-		}
+		let preference = await preferences.addNewPreference(user_id, genre_id)
 		res.status(200).json({
             status: 'success',
-            message: 'Preferences added.'
+			message: 'Preferences added.',
+			payload: preference
         });
 	}
 	catch(err) {
