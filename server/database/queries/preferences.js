f713eab7f046821b713005d36ffaa097862ff8fa
kadjiah <kadijahwilson@pursuit.org>
2020-05-23 17:27:17 -0400

    added select statement for preference route

--- server/database/queries/preferences.js
+++ server/database/queries/preferences.js
@@ -1,9 +1,13 @@
 const db = require('../db.js')
 
 
-getPreferenceByGenreId = async (user_id) => {
+const getPreferenceByUserId = async (user_id) => {
     try{
-		let preferenceQuery = 'SELECT * FROM preferences INNER JOIN genres ON preferences.genre_id = genres.id WHERE user_id = $1';
+        let preferenceQuery = 
+       `SELECT * FROM preferences 
+        INNER JOIN genres 
+        ON preferences.genre_id = genres.id 
+        WHERE user_id = $1`;
         let preferences = await db.any(preferenceQuery, [user_id])
         return preferences
     }catch(err){
@@ -11,7 +15,21 @@ getPreferenceByGenreId = async (user_id) => {
     }
 }
 
+//show check if user likes that genre && insert if they don't have it preferences
+const addNewPreference = async (user_id, genre_id) => {
+try{
+    let preferenceQuery = 'SELECT * FROM preferences WHERE user_id = $1 AND genre_id = $2';
+    let preferenceInsertQuery = 'INSERT INTO preferences (user_id, genre_id) VALUES ($1, $2)'
+    let alreadyPreference = await db.oneOrNone(preferenceQuery, [user_id, genre_id])
+    if (!alreadyPreference) {
+        await db.none(preferenceInsertQuery, [user_id, genre_id]);
+    }
+}catch(err){
+    console.log(err)
+}
+}
 
 module.exports = {
-getPreferenceByGenreId
+getPreferenceByUserId,
+addNewPreference
 }
