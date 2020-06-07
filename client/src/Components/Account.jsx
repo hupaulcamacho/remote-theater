a59ddda90f9378f997042697cde4b3a27da455d4
kadjiah <kadijahwilson@pursuit.org>
2020-06-07 12:09:33 -0400

    Checkboxes with errors

--- client/src/Components/Account.jsx
+++ client/src/Components/Account.jsx
@@ -12,7 +12,7 @@ class Account extends Component {
             userPreferences: null,
             genres: null,
             message: '', 
-            setMessage:''
+            checkBox: false
         }
     }
 
@@ -21,11 +21,11 @@ class Account extends Component {
         await this.getUserPreferences()
     }
 
-    notify = (e) => {
-        console.log(e.target.innerText)
-        let genre = e.target.innerText
-        toast.success(`${genre} was added to preferences.`)
-    }
+    // notify = (e) => {
+    //     console.log(e.target.innerText)
+    //     let genre = e.target.innerText
+    //     toast.success(`${genre} was added to preferences.`)
+    // }
     
 
     getAllGenres = async () => {
@@ -49,7 +49,7 @@ class Account extends Component {
     }
 
     addToPreferences = async (e) => {
-        const { user} = this.state
+        const { user } = this.state
         const genreId = e.target.id
         console.log(genreId)
         let genre = e.target.innerText
@@ -57,44 +57,58 @@ class Account extends Component {
         const URL = `/api/preferences/add/${user.id}/${genreId}`
         await axios.post(URL)
 
-        // this.setState({
-        //     setMessage: "added preference!"
-        // })
-
-        toast.success(`${genre} was added to preferences.`)
-        this.getUserPreferences()
+        // toast.success(`${genre} was added to preferences.`)
+        // this.getUserPreferences()
     }
 
     deletePreference = async (e) => {
-        const { user} = this.state
+        const { user } = this.state
         const genreId = e.target.id
+
         const URL = `/api/preferences/delete/${user.id}/${genreId}`
         let genre = e.target.innerText
         await axios.delete(URL)
-        // this.setState({
-        //     setMessage: "deleted preference!"
-        // })
-        toast.error(`${genre} was removed from preferences.`)
-        await this.getUserPreferences()
+
+        // toast.error(`${genre} was removed from preferences.`)
+        // await this.getUserPreferences()
     }
 
+
+clickPreference = async (e) => {
+    const { checkBox } = this.state
+    console.log("here")
+    if(checkBox.checked){
+        this.setState({ checkBox: true})
+        console.log('checked')
+        this.addToPreferences()
+    }
+}
     render() {
-        const { user, genres, userPreferences, setMessage } = this.state
+        const { user, genres, userPreferences } = this.state
         const userPreferenceComponents = [];
         const genreComponents = [];
+
         for (let i = 0; i < genres?.length; i ++) {
-            // console.log(genres[i].name)
             genreComponents.push(
                 <p onClick={this.addToPreferences} className='genre' id={genres[i].id} name={genres[i].name}>{genres[i].name}</p>
             )
         }
+
         for (let i = 0; i < userPreferences?.length; i ++) {
             console.log(userPreferences[i].name)
             userPreferenceComponents.push(
                 <p onClick={this.deletePreference}className='genre2' id={userPreferences[i].id}>{userPreferences[i].name}</p>
                 )
-
         }
+
+        let genreOptions = genreComponents.map(el => ( 
+            <div>
+            <label> {el.props.name} </label>
+            <input name="checkBox" type="checkbox" onClick={this.clickPreference()} />
+            </div>
+            
+        ))
+
         return (
             <div>
                 <h1>Account</h1>
@@ -104,18 +118,16 @@ class Account extends Component {
                     <h2>My Preferences</h2>
                     <div className='genre-container'>
                         {userPreferenceComponents}
-                        <p className="message">{setMessage}</p>
 
                     </div>
                     <Popup trigger={<button className="button"> Change Preferences </button>} modal closeOnDocumentClick>
                         <h2>Change Preferences</h2>
                         <div className='genre-container'>
-                            {genreComponents}
-                        <p className="message">{setMessage}</p>
+                            {/* {genreComponents} */}
+                           {genreOptions}
                         </div>
                     </Popup>
-                </div>
-                
+                </div>  
             </div>
         )
     }
