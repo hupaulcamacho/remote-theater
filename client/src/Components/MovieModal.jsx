a4db32e11e5ac32883cdf1b91f31c792dd72fbcb
Hupaul <hupaulcamacho@pursuit.org>
2020-06-12 13:18:49 -0400

    heroku version

--- client/src/Components/MovieModal.jsx
+++ client/src/Components/MovieModal.jsx
@@ -64,7 +64,7 @@ class MovieModal extends Component {
                                 </div>
                                 <div className = 'copyPaste'>
                                     <textarea   ref={(textarea) => this.textArea = textarea}
-                                    value={`localhost:3000/privateroom/${movie ?.video.video_url}/${movie ?.video.title}_${privateId} /${difference}/private`} className='greyLink'></textarea>
+                                    value={`remotetheater.herokuapp.com/privateroom/${movie ?.video.video_url}/${movie ?.video.title}_${privateId} /${difference}/private`} className='greyLink'></textarea>
                                     <Link className='movie-link' onClick={() => this.copyCodeToClipboard()}>
                                          Copy to Clipboard
                                     </Link>
